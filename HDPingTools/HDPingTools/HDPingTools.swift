da6be223c45ae6470eb25821318f57c955fe6a40
Damon <dong765@qq.com>
2021-01-04 16:16:40 +0800

    Initial commit

---
+++ HDPingTools/HDPingTools/HDPingTools.swift
@@ -0,0 +1,174 @@
+//
+//  HDPingTools.swift
+//  HDPingTools
+//
+//  Created by Damon on 2021/1/4.
+//
+
+import Foundation
+
+public typealias PingComplete = ((_ response: HDPingResponse?, _ error: Error?) -> Void)
+
+public enum HDPingError: Error, Equatable {
+    case requestError   //发起失败
+    case receiveError  //响应失败
+    case receiveUnexpectedPacket
+    case timeOut
+}
+
+public struct HDPingResponse {
+    public var pingAddressIP = ""
+    public var responseTime: TimeInterval = 0
+    public var responseBytes: Int = 0
+}
+
+open class HDPingTools: NSObject {
+
+    public var timeOut: TimeInterval = 10  //超时时间，毫秒
+    private(set) var isPing = false
+
+    private var pinger: SimplePing
+    private var complete: PingComplete?
+    private var sendStartTime: Date?
+    private var sendTimer: Timer?
+    private var sendInterval: TimeInterval = 0
+    private var pingAddressIP = ""
+
+    public init(hostName: String) {
+        pinger = SimplePing(hostName: hostName)
+        super.init()
+        pinger.delegate = self
+    }
+
+
+    /// 开始ping请求
+    /// - Parameters:
+    ///   - pingType: ping的类型
+    ///   - interval: 是否重复定时ping
+    ///   - complete: 请求的回调
+    public func start(pingType: SimplePingAddressStyle = .any, interval: TimeInterval = 0, complete: PingComplete? = nil) {
+        self.stop()
+        self.complete = complete
+        self.pinger.addressStyle = pingType
+        self.sendInterval = interval
+        self.pinger.start()
+        self.isPing = true
+    }
+
+    public func stop() {
+        self.pinger.stop()
+        self.isPing = false
+        sendTimer?.invalidate()
+        sendTimer = nil
+        sendStartTime = nil
+    }
+}
+
+private extension HDPingTools {
+    func sendPingData() {
+        if let sendStartTime = sendStartTime {
+            let time = Date().timeIntervalSince(sendStartTime).truncatingRemainder(dividingBy: 1) * 1000
+            if time > self.timeOut,  let complete = self.complete {
+                complete(nil, HDPingError.timeOut)
+            }
+        } else {
+            pinger.send(with: nil)
+        }
+    }
+
+    func displayAddressForAddress(address: NSData) -> String {
+        var hostStr = [Int8](repeating: 0, count: Int(NI_MAXHOST))
+
+        let success = getnameinfo(
+            address.bytes.assumingMemoryBound(to: sockaddr.self),
+            socklen_t(address.length),
+            &hostStr,
+            socklen_t(hostStr.count),
+            nil,
+            0,
+            NI_NUMERICHOST
+        ) == 0
+        let result: String
+        if success {
+            result = String(cString: hostStr)
+        } else {
+            result = "?"
+        }
+        return result
+    }
+
+    func shortErrorFromError(error: NSError) -> String {
+        if error.domain == kCFErrorDomainCFNetwork as String && error.code == Int(CFNetworkErrors.cfHostErrorUnknown.rawValue) {
+            if let failureObj = error.userInfo[kCFGetAddrInfoFailureKey as String] {
+                if let failureNum = failureObj as? NSNumber {
+                    if failureNum.intValue != 0 {
+                        let f = gai_strerror(Int32(failureNum.intValue))
+                        if f != nil {
+                            return String(cString: f!)
+                        }
+                    }
+                }
+            }
+        }
+        if let result = error.localizedFailureReason {
+            return result
+        }
+        return error.localizedDescription
+    }
+}
+
+extension HDPingTools: SimplePingDelegate {
+    public func simplePing(_ pinger: SimplePing, didStartWithAddress address: Data) {
+        pingAddressIP = self.displayAddressForAddress(address: NSData(data: address))
+        print("ping: ", pingAddressIP)
+        self.sendPingData()
+        if sendInterval > 0 && sendTimer == nil {
+            sendTimer = Timer(timeInterval: sendInterval, repeats: true, block: { [weak self] (_) in
+                guard let self = self else { return }
+                self.sendPingData()
+            })
+            RunLoop.main.add(sendTimer!, forMode: .common)
+        }
+    }
+
+    public func simplePing(_ pinger: SimplePing, didFailWithError error: Error) {
+        print("ping failed: ", self.shortErrorFromError(error: error as NSError))
+        if let complete = self.complete {
+            complete(nil, HDPingError.requestError)
+        }
+        self.stop()
+    }
+
+    public func simplePing(_ pinger: SimplePing, didSendPacket packet: Data, sequenceNumber: UInt16) {
+        print("ping sent \(packet.count) data bytes, icmp_seq=\(sequenceNumber)")
+        sendStartTime = Date()
+    }
+
+    public func simplePing(_ pinger: SimplePing, didFailToSendPacket packet: Data, sequenceNumber: UInt16, error: Error) {
+        print("ping send error: ", sequenceNumber, self.shortErrorFromError(error: error as NSError))
+        sendStartTime = nil
+        if let complete = self.complete {
+            complete(nil, HDPingError.receiveError)
+        }
+    }
+
+    public func simplePing(_ pinger: SimplePing, didReceivePingResponsePacket packet: Data, sequenceNumber: UInt16) {
+        if let sendTime = sendStartTime {
+            let time = Date().timeIntervalSince(sendTime).truncatingRemainder(dividingBy: 1) * 1000
+            print("\(packet.count) bytes from \(pingAddressIP): icmp_seq=\(sequenceNumber) time=\(time)ms")
+            sendStartTime = nil
+            if let complete = self.complete {
+                let response = HDPingResponse(pingAddressIP: pingAddressIP, responseTime: time, responseBytes: packet.count)
+                complete(response, nil)
+            }
+        }
+    }
+
+    public func simplePing(_ pinger: SimplePing, didReceiveUnexpectedPacket packet: Data) {
+        print("unexpected packet, size=\(packet.count)")
+        sendStartTime = nil
+        if let complete = self.complete {
+            complete(nil, HDPingError.receiveUnexpectedPacket)
+        }
+    }
+}
---
+++ HDPingTools/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  HDPingTools
+//
+//  Created by Damon on 2021/1/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    let pingTools = HDPingTools(hostName: "www.apple.com")
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+        let button = UIButton(type: .custom)
+        button.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        button.backgroundColor = UIColor.red
+        self.view.addSubview(button)
+        button.addTarget(self, action: #selector(start), for: .touchUpInside)
+
+    }
+
+    @objc func start() {
+        if pingTools.isPing {
+            pingTools.stop()
+        } else {
+            pingTools.start(pingType: .any, interval: 0) { (response, error) in
+                print(response?.pingAddressIP ?? "")
+            }
+        }
+    }
+
+    
+}
+
